cd1a7a05074aaf6670d8d13edbed743a938ebbf0
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-08-21 16:59:24 +0530

    Add files via upload

---
+++ 18 xxx.js
@@ -0,0 +1,50 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, text, command }) => {
+try {
+let res = await fetch("https://api-fgmods.ddns.net/api/downloader/xvideosdl?url=${text}&apikey=oJUeCkMM");
+let tech = await res.json()
+
+let meka = `
+ ◈━━━━━━━━━━━━━◈
+     *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*               
+◈━━━━━━━━━━━━━◈
+
+*title🔞_*  ${tech.result.title}
+
+*👁️‍🗨️keyword_*  ${tech.result.keyword}
+
+*🧐viwes_*  ${tech.result.views}
+
+*🪙vote_*  ${tech.result.vote}
+
+*like👍_*  ${tech.result.likes}
+
+*deslike👎_*  ${tech.result.deslikes}
+
+*Image🏖️_*  ${tech.result.thumb}
+░░░░░░░░░░░░░░░░░░░░░░`
+
+let title = `${tech.result.title}`
+let url = `${tech.result.url}`
+
+conn.sendMessage(m.chat, { react: { text: `🔞`, key: m.key }})
+
+await conn.sendMessage(m.chat, { text: `*⏳𝙋𝙇𝘼𝙎𝙀 𝙒𝘼𝙄𝙏⏳*` } ,m)
+
+conn.sendFile(m.chat, url, title + '.mp4', meka.trim(), m, false, { asDocument: chat.useDocument })
+} catch {
+
+await conn.sendMessage(m.chat, { text: `*මොන මගුලක්ද බන් හදනකන් හිටපන්කෝ😂😂*` } ,m)
+
+}
+}
+
+
+handler.help = ['sex','xv']
+handler.tags = ['main']
+handler.command = /^(sex|xv)$/i
+handler.premium = true
+
+export default handler
+
